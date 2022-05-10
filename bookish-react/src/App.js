9d78aeb606adaa81b046925e25d351286055ba4a
Evgeny Vasilyev <e.vasilyev@dodopizza.com>
2022-05-10 16:34:02 +0300

    extract BooksListContainer component

--- bookish-react/src/App.js
+++ bookish-react/src/App.js
@@ -1,36 +1,14 @@
 import Typography from "@material-ui/core/Typography";
-import axios from "axios";
-import { useEffect, useState } from "react";
 
-import { BooksList } from "./BooksList";
+import { BooksListContainer } from "./BooksListContainer";
 
 function App() {
-  const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    const fetchBooks = async () => {
-      setError(false);
-      setLoading(true);
-      try {
-        const result = await axios.get("http://localhost:8080/books");
-        setBooks(result.data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchBooks();
-  }, []);
-
   return (
     <div className="App">
       <Typography variant="h2" component="h2" data-test="heading">
         Bookish
       </Typography>
-      <BooksList books={books} />
+      <BooksListContainer />
     </div>
   );
 }
