bd554e70cd9f23116a70192e2cf1f61bcfb27dca
Evgeny Vasilyev <e.vasilyev@dodopizza.com>
2022-05-10 12:56:32 +0300

    use stub server to fetch data

--- bookish-react/src/App.js
+++ bookish-react/src/App.js
@@ -1,9 +1,18 @@
 import Typography from "@material-ui/core/Typography";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 import { BooksList } from "./BooksList";
 
 function App() {
-  const books = ["Refactoring", "Domain-driven design"];
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const result = await axios.get("http://localhost:8080/books");
+      setBooks(result.data);
+    };
+    fetchBooks();
+  }, []);
 
   return (
     <div className="App">
--- bookish-react/src/BooksList.js
+++ bookish-react/src/BooksList.js
@@ -3,7 +3,7 @@ export function BooksList({ books }) {
     <div data-test="book-list">
       {books.map((book) => (
         <div className="book-item">
-          <h2>{book}</h2>
+          <h2>{book.name}</h2>
         </div>
       ))}
     </div>
