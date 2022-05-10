59924ed9e00b943bbcc44548316861ca02284c9a
Evgeny Vasilyev <e.vasilyev@dodopizza.com>
2022-05-10 21:12:24 +0300

    BookDetail renders description

--- bookish-react/src/BookDetail.js
+++ bookish-react/src/BookDetail.js
@@ -1,7 +1,7 @@
 export function BookDetail({ book }) {
   return (
     <div className="book-detail">
-      <h2 className="book-title">{book.name}</h2>;
+      <h2 className="book-title">{book.name}</h2>;<p className="book-description">{book.description}</p>
     </div>
   );
 }
--- bookish-react/src/BookDetail.test.js
+++ bookish-react/src/BookDetail.test.js
@@ -12,6 +12,21 @@ describe("BookDetail", () => {
 
     render(<BookDetail {...props} />);
 
-    expect(screen.getByText(/Refactoring/)).toBeInTheDocument();
+    expect(screen.getByText(props.book.name)).toBeInTheDocument();
+  });
+
+  it("renders description", () => {
+    const props = {
+      book: {
+        id: 1,
+        name: "Refactoring",
+        description:
+          "Martin Fowler's Refactoring defined core ideas and techniques that hundreds of thousands of developers have used to improve their software.",
+      },
+    };
+
+    render(<BookDetail {...props} />);
+
+    expect(screen.getByText(props.book.description)).toBeInTheDocument();
   });
 });
