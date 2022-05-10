e99addc4f1d485cf83a8817a80c2d21f16ac7cd1
Evgeny Vasilyev <e.vasilyev@dodopizza.com>
2022-05-10 22:01:25 +0300

    remove ;

--- bookish-react/src/BookDetail/BookDetail.js
+++ bookish-react/src/BookDetail/BookDetail.js
@@ -1,7 +1,8 @@
 export function BookDetail({ book }) {
   return (
     <div className="book-detail">
-      <h2 className="book-title">{book.name}</h2>;<p className="book-description">{book.description}</p>
+      <h2 className="book-title">{book.name}</h2>
+      <p className="book-description">{book.description}</p>
     </div>
   );
 }
