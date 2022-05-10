56fc9a7a19bde1db8ac1e4b0db6e170ebf9e5b4e
Evgeny Vasilyev <e.vasilyev@dodopizza.com>
2022-05-10 20:55:42 +0300

    generalize useRemoteService hook

--- bookish-react/src/BooksListContainer.js
+++ bookish-react/src/BooksListContainer.js
@@ -2,7 +2,7 @@ import { BooksList } from "./BooksList";
 import { useRemoteService } from "./hooks";
 
 export function BooksListContainer() {
-  const { books, loading, error } = useRemoteService();
+  const { books, loading, error } = useRemoteService("http://localhost:8080/books", []);
 
   return <BooksList books={books} loading={loading} error={error} />;
 }
--- bookish-react/src/hooks.js
+++ bookish-react/src/hooks.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export function useRemoteService() {
-  const [books, setBooks] = useState([]);
+export function useRemoteService(url, initialData) {
+  const [books, setBooks] = useState(initialData);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -11,7 +11,7 @@ export function useRemoteService() {
       setError(false);
       setLoading(true);
       try {
-        const result = await axios.get("http://localhost:8080/books");
+        const result = await axios.get(url);
         setBooks(result.data);
       } catch (error) {
         setError(error);
