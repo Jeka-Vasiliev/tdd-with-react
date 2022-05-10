d4f5fe01ab2cad884d1177065ffa36b8c5cbcb1a
Evgeny Vasilyev <e.vasilyev@dodopizza.com>
2022-05-10 16:28:29 +0300

    add new test book

--- bookish-react/cypress/integration/ui/bookish.spec.js
+++ bookish-react/cypress/integration/ui/bookish.spec.js
@@ -11,14 +11,9 @@ describe("Bookish application", () => {
   });
   beforeEach(async () => {
     const books = [
-      {
-        id: 1,
-        name: "Refactoring",
-      },
-      {
-        id: 2,
-        name: "Domain-driven design",
-      },
+      { id: 1, name: "Refactoring" },
+      { id: 2, name: "Domain-driven design" },
+      { id: 3, name: "Building Microservices" },
     ];
     for (const book of books) {
       await axios.post("http://localhost:8080/books", book, { headers: { "Content-Type": "application/json" } });
@@ -34,10 +29,10 @@ describe("Bookish application", () => {
     cy.visit("http://localhost:3000");
     cy.get('div[data-test="book-list"]').should("exist");
     cy.get("div.book-item").should((books) => {
-      expect(books).to.have.length(2);
+      expect(books).to.have.length(3);
 
       const titles = [...books].map((b) => b.querySelector("h2").innerHTML);
-      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design"]);
+      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design", "Building Microservices"]);
     });
   });
 });
