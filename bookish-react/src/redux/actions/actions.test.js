95f296034d092759c2cd8992399ce6adf5fe2606
Evgeny Vasilyev <e.vasilyev@dodopizza.com>
2022-05-11 09:46:38 +0300

    extract action types

--- bookish-react/src/redux/actions/actions.test.js
+++ bookish-react/src/redux/actions/actions.test.js
@@ -2,6 +2,7 @@ import { setSearchTerm, fetchBooks } from "./actions";
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import axios from "axios";
+import * as types from "./types";
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -10,7 +11,7 @@ describe("BookListContainer related actions", () => {
   it("Sets the search keyword", () => {
     const term = "";
     const expected = {
-      type: "SET_SEARCH_TERM",
+      type: types.SET_SEARCH_TERM,
       term,
     };
 
@@ -25,7 +26,7 @@ describe("BookListContainer related actions", () => {
       { id: 2, name: "Domain-driven design" },
     ];
     axios.get = jest.fn().mockImplementation(() => Promise.resolve({ data: books }));
-    const expectedActions = [{ type: "FETCH_BOOKS_PENDING" }, { type: "FETCH_BOOKS_SUCCESS", books }];
+    const expectedActions = [{ type: types.FETCH_BOOKS_PENDING }, { type: types.FETCH_BOOKS_SUCCESS, books }];
     const store = mockStore({ books: [] });
     await store.dispatch(fetchBooks(""));
 
@@ -35,8 +36,8 @@ describe("BookListContainer related actions", () => {
   it("Fetch data with error", async () => {
     axios.get = jest.fn().mockImplementation(() => Promise.reject({ message: "Something went wrong" }));
     const expectedActions = [
-      { type: "FETCH_BOOKS_PENDING" },
-      { type: "FETCH_BOOKS_FAILED", err: "Something went wrong" },
+      { type: types.FETCH_BOOKS_PENDING },
+      { type: types.FETCH_BOOKS_FAILED, err: "Something went wrong" },
     ];
     const store = mockStore({ books: [] });
 
