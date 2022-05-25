50f7cf9d4bcbef79c90515d343726f975722c064
Evgeny Vasilyev <e.vasilyev@dodopizza.com>
2022-05-25 10:20:56 +0300

    remove unused hook

--- bookish-react/src/hooks.js
+++ bookish-react/src/hooks.js
@@ -1,31 +1,4 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core";
-
-export function useRemoteService(initialUrl, initialData) {
-  const [data, setData] = useState(initialData);
-  const [url, setUrl] = useState(initialUrl);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setError(false);
-      setLoading(true);
-      try {
-        const result = await axios.get(url);
-        setData(result.data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [url]);
-
-  return { data, loading, error, setUrl };
-}
 
 export const useStyles = makeStyles((theme) => ({
   root: {
