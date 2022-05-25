8eb61ab8d79b536134ab4de36519dafcceed0943
Evgeny Vasilyev <e.vasilyev@dodopizza.com>
2022-05-25 10:25:52 +0300

    ReviewList renders a list when data is passed

--- bookish-react/src/ReviewList/ReviewList.js
+++ bookish-react/src/ReviewList/ReviewList.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export const ReviewList = () => {
-  return <div data-testid="reviews-container"></div>;
+export const ReviewList = ({ reviews }) => {
+  return (
+    <div data-testid="reviews-container">
+      {reviews.map((review) => (
+        <div key={review.name + review.date} data-testid="review">
+          {review.name}
+        </div>
+      ))}
+    </div>
+  );
 };
--- bookish-react/src/ReviewList/ReviewList.test.js
+++ bookish-react/src/ReviewList/ReviewList.test.js
@@ -14,4 +14,18 @@ describe("ReviewList", () => {
 
     expect(reviews).toBeInTheDocument();
   });
+
+  it("renders a list when data is passed", () => {
+    const props = {
+      reviews: [
+        { name: "Juntao", date: "2018/06/21", content: "Excellent work, really impressed by your efforts" },
+        { name: "Abruzzi", date: "2018/06/22", content: "What a great book" },
+      ],
+    };
+    render(<ReviewList {...props} />);
+
+    const reviews = screen.getAllByTestId("review");
+
+    expect(reviews.length).toBe(2);
+  });
 });
